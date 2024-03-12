d9e563569e562c550e1e2ac70d4cc9412bdf8e57
WeiLing Pan <zz860530@gmail.com>
2024-03-12 22:52:03 +0800

    feature: set uvicorn worker

--- app_fastapi/main.py
+++ app_fastapi/main.py
@@ -33,6 +33,6 @@ async def error():
     return {"test": "error"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    #uvicorn main:app --reload
+    uvicorn.run("main:app", host='0.0.0.0', port=8000, workers=2)
+    #python main.py
     #conda env export > environment.yml
